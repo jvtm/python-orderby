d8547b2c2ffce1bf24b62be00f0280670d784c22
Jyrki Muukkonen <jvtm@kruu.org>
2016-11-10 20:48:05 +0200

    lets call it a 0.0.2

--- orderby/__init__.py
+++ orderby/__init__.py
@@ -1,4 +1,5 @@
 from .sorter import asc, desc, orderby
 
 __all__ = ['asc', 'desc', 'orderby']
-__version__ = '0.0.1'
+__version__ = '0.0.2'
+
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='orderby',
-    version='0.0.1',
+    version='0.0.2',
     description='Python key functions for multi-field ordering',
     long_description=long_description,
     url='https://github.com/jvtm/python-orderby',
